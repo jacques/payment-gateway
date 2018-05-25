138a8b3cfc13437815be0bde8c95ad5fe302daef
Jacques Marneweck <jacques@powertrip.co.za>
2018-05-25 21:24:33 +0000

    Prep for dev

---
+++ src/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Payment Gateway
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018 Jacques Marneweck.  All right strictly reserved.
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
